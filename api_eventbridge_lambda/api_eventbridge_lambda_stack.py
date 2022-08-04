2124c0b7d3b7d10f978864999e9830e48a038239
HakimiX <mustafa.hakimi@outlook.dk>
2022-08-04 18:30:17 +0200

    add event producer lambda

--- api_eventbridge_lambda/api_eventbridge_lambda_stack.py
+++ api_eventbridge_lambda/api_eventbridge_lambda_stack.py
@@ -1,19 +1,17 @@
 from aws_cdk import (
-    # Duration,
+    aws_lambda,
     Stack,
-    # aws_sqs as sqs,
 )
 from constructs import Construct
+
 
 class ApiEventbridgeLambdaStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ApiEventbridgeLambdaQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Producer Lambda
+        event_producer_lambda = aws_lambda.Function(self, 'eventProducerLambda',
+                                                    runtime=aws_lambda.Runtime.PYTHON_3_8,
+                                                    handler="event_producer_lambda.lambda_handler",
+                                                    code=aws_lambda.Code.from_asset('lambda'))
