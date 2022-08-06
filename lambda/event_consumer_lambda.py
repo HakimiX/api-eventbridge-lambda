41d57ac8092a678a1d3f1fdce861ce2d34448180
HakimiX <mustafa.hakimi@outlook.dk>
2022-08-06 13:01:24 +0200

    add doc-string for consumer lambda payload

--- lambda/event_consumer_lambda.py
+++ lambda/event_consumer_lambda.py
@@ -6,6 +6,15 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+""" 
+Received EventBridge event: 
+{
+    "Time": "2019-12-04",
+    "Source": "com.mycompany.myapp",
+    "Detail": "something...",
+    "DetailType": "service_status"
+}
+"""
 
 def lambda_handler(event, context):
   logger.info(event)
