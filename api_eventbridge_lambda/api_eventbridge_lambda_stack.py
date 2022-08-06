d5f6d585b6945b71c71ab7bcb07b44f57d038130
HakimiX <mustafa.hakimi@outlook.dk>
2022-08-06 12:45:22 +0200

    add iac for producer lambda and iam role

--- api_eventbridge_lambda/api_eventbridge_lambda_stack.py
+++ api_eventbridge_lambda/api_eventbridge_lambda_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_lambda,
+    aws_iam as iam,
     Stack,
 )
 from constructs import Construct
@@ -10,8 +11,23 @@ class ApiEventbridgeLambdaStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Producer Lambda
+        """
+        Producer Lambda function with IAM policy (event_policy) attached so it can send events to EventBridge.
+        The Lambda function is triggered by the API Gateway. 
+        """
         event_producer_lambda = aws_lambda.Function(self, 'eventProducerLambda',
                                                     runtime=aws_lambda.Runtime.PYTHON_3_8,
                                                     handler="event_producer_lambda.lambda_handler",
                                                     code=aws_lambda.Code.from_asset('lambda'))
+
+        # IAM Policy so Lambda can send events to EventBridge
+        event_policy = iam.PolicyStatement(effect=iam.Effect.ALLOW,
+                                           resources=['*'],
+                                           actions=['events:PutEvents'])
+
+        # Attach policy to event_producer_lambda
+        event_producer_lambda.add_to_role_policy(event_policy)
+        
+        """
+        Consumer Lambda function #1 with event rule so it can receive events from EventBridge. 
+        """
---
+++ lambda/event_producer_lambda.py
@@ -0,0 +1,39 @@
+import logging
+import datetime
+import boto3
+import json
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+  eventbridge_client = boto3.client('events')
+  request_body = event['body']
+  if request_body is None:
+    request_body = ""
+  
+  # Structure of EventBridge Event
+  eventbridge_event = {
+    'Time': datetime.datetime.now(),
+    'Source': 'com.mycompany.myapp',
+    'Detail': request_body,
+    'DetailType': 'service_status'
+  }
+  logger.info(eventbridge_event)
+
+  # Send event to EventBridge
+  response = eventbridge_client.put_events(
+    Entries=[
+      eventbridge_event
+    ]
+  )
+  logger.inof(response)
+
+  # Return success response to API Gateway
+  return {
+    'statusCode': 200,
+    'body': json.dumps({
+      'result': 'from producer'
+    })
+  }
